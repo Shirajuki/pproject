7da9ee11ac4a7adb48dd0deb518141f821fd3fb7
shirajuki <vulturegfd@reborn.com>
2019-10-30 20:10:34 +0100

    a

--- js/calendar.js
+++ js/calendar.js
@@ -14,7 +14,13 @@ const calendar = [
   {type: "practice", title: "", informasjon: "Drifting. Instructor: Harald Gress", dato: "2019-10-12", tid: "18:00", sted: "Gløshaugen", link:"javascript:void(0)", tag: ""},
   {type: "event", title: "Drifting fun!", informasjon: "Drifting. Instructor: Harald Gress Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse ex nam quo reprehenderit. Ad alias amet at blanditiis cupiditate debitis delectus ea eligendi eum facilis inventore qui repellendus unde, vel. Lorem ipsum dolor sit amet, consectetur adipisicing elit.", dato: "2020-01-06", tid: "18:00", sted: "Solsiden", link:"javascript:void(0)", tag: ""},
 ];
-
+Date.prototype.getWeekNumber = function(){
+   var d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
+   var dayNum = d.getUTCDay() || 7;
+   d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+   var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+   return Math.ceil((((d - yearStart) / 86400000) + 1)/7)
+ };
 function loadCalendar(calendarList) {
 	calendarList = sortByDate(calendarList)
 	const calendarId = document.getElementById("calendarElements")
@@ -51,14 +57,6 @@ function loadCalendar(calendarList) {
 	string += `</ul></div>`
 	calendarId.innerHTML += string
 }
-
-Date.prototype.getWeekNumber = function() {
-  let d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
-  let dayNum = d.getUTCDay() || 7;
-  d.setUTCDate(d.getUTCDate() + 4 - dayNum);
-  let yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
-  return Math.ceil((((d - yearStart) / 86400000) + 1)/7)
-};
 
 function checkType(object) {
   return function(element) {
