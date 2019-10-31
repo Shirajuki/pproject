0f572a21542c987b039bbffa531c291a1197c386
ingsovik <ingsovik@users.noreply.github.com>
2019-10-31 21:20:15 +0100

    funk

--- js/navscroll.js
+++ js/navscroll.js
@@ -1,3 +1,10 @@
+Date.prototype.getWeekNumber = function() {
+  let d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
+  let dayNum = d.getUTCDay() || 7;
+  d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+  let yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+  return Math.ceil((((d - yearStart) / 86400000) + 1)/7)
+};
 // Mobile Responsive dropdown navigation - JONNY CAN FIX THIS!
 function myFunction() {
   setTimeout(() => {
