c79998639786bdfd7735f276323b160238eacf0b
ingsovik <ingsovik@users.noreply.github.com>
2019-11-01 21:39:58 +0100

    laget tekst til events og sånt, + noen flere practicer

--- js/data.js
+++ js/data.js
@@ -1,22 +1,36 @@
 const calendar = [
 	{
-		type: "event",
-		title: "Welcome Party!",
-		informasjon: "Welcome party for new members! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse ex nam quo reprehenderit. Ad alias amet at blanditiis cupiditate debitis delectus ea eligendi eum facilis inventore qui repellendus unde, vel. Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-		dato: "2019-10-05",
-		tid: "14:15",
-		sted: "Solsiden",
-		link:"2019-10-05-welcome-party",
-		tag: "Social"
-	},
-	{type: "practice", title: "Group Practice #32", informasjon: "Drifting. Instructor: Harald Gress", dato: "2019-10-06", tid: "18:00", sted: "Gløshaugen", link:"javascript:void(0)", tag: "Practice"},
-	{type: "practice", title: "Group Practice #33", informasjon: "Drifting. Instructor: Harald Gress", dato: "2019-10-07", tid: "18:00", sted: "Tyholt", link:"javascript:void(0)", tag: "Practice"},
-	{type: "practice", title: "Group Practice #34", informasjon: "Drifting. Instructor: Harald Gress", dato: "2019-10-12", tid: "18:00", sted: "Gløshaugen", link:"javascript:void(0)", tag: "Practice"},
-	{type: "event", title: "Drifting fun!", informasjon: "Drifting. Instructor: Harald Gress Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse ex nam quo reprehenderit. Ad alias amet at blanditiis cupiditate debitis delectus ea eligendi eum facilis inventore qui repellendus unde, vel. Lorem ipsum dolor sit amet, consectetur adipisicing elit.", dato: "2020-01-06", tid: "18:00", sted: "Solsiden", link:"javascript:void(0)", tag: "Social"},
-	{type: "event", title: "Party at mine", informasjon: "We will have a great party at mine (near Gløshaugen) next friday in order to celebrate our great efforts so far this year", dato: "2019-10-29", tid: "18:00", sted: "Gløshaugen", link:"javascript:void(0)", tag: "Social"},
-	{type: "event", title: "Lets make some money", informasjon: "During this week we have a fundraising event to raise some money for the club. More info to come", dato: "2019-10-30", tid: "18:00", sted: "Gløshaugen", link:"javascript:void(0)", tag: "Fundraiser"},
-	{type: "event", title: "Big competition in OSLO", informasjon: "The big racer competition is being held in Oslo on the 1.11.19. More information to come soon your way", dato: "2019-10-30", tid: "18:00", sted: "Oslo", link:"javascript:void(0)", tag: "Competition"},
+		type: "event", title: "Welcome Party!", informasjon: "Welcome party for new members! Its been some time since school started again, but  its never to late to have a welcome party to get to know the new members of the team! Lets gather to have a good time, meet new people, and party the night away. Make sure to bring some good vibes. PS: puking should happen outside.",
+		dato: "2019-10-05", tid: "14:15", sted: "Solsiden", link:"2019-10-05-welcome-party", tag: "Social"},
+	{type: "practice", title: "Group Practice #17", informasjon: "Today: Tweaking. Instructor: Harald Gress",
+		dato: "2019-10-16", tid: "18:00", sted: "Gløshaugen", link:"javascript:void(0)", tag: "Practice"},
+	{type: "practice", title: "Group Practice #16", informasjon: "Today: Speed. Instructor: Harald Gress",
+		dato: "2019-10-13", tid: "18:00", sted: "Tyholt", link:"javascript:void(0)", tag: "Practice"},
+	{type: "practice", title: "Group Practice #15", informasjon: "Today: Drifting. Instructor: Harald Gress",
+		dato: "2019-10-09", tid: "18:00", sted: "Gløshaugen", link:"javascript:void(0)", tag: "Practice"},
+	{type: "event", title: "Drifting fun!", informasjon: "Drifting spectacular. Instructor: Harald Gress Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse ex nam quo reprehenderit. Ad alias amet at blanditiis cupiditate debitis delectus ea eligendi eum facilis inventore qui repellendus unde, vel. Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+		dato: "2020-01-06", tid: "18:00", sted: "Solsiden", link:"javascript:void(0)", tag: "Social"},
+	{type: "event", title: "Party at mine", informasjon: "It's that time of year again! No, not Christmas (yet). ITS HALLOWEEN. THE SPOOKIES TIME OF YEAR. And it's time to party. It's my honor to invite the entire team  home to me and party the spooky night away into christmas! \n" +
+			"Dress code: costumes (spooky or sexy or funny, everyone is accepted)",
+		dato: "2019-10-31", tid: "19:00", sted: "Harald Gress' Home", link:"javascript:void(0)", tag: "Social"},
+	{type: "event", title: "Lets make some money", informasjon: "Time to make some money for this rockin' team!\n" +
+			"During this Sunday a fundraiser will be held. We'll sell cakes, waffles, coffee and 'lodd' where you can win some sweet prices! Invite your friends and family. Hope to see you there. ",
+		dato: "2019-10-30", tid: "18:00", sted: "Gløshaugen", link:"javascript:void(0)", tag: "Fundraiser"},
+	{type: "event", title: "Big competition in Oslo", informasjon: "The coming week there will be held the annual Norwegian Winter Lawnracers Race  (NWLR) for the 3rd time. The group registered to compete will travel together by train on Friday, whilst the lawnmowers will be shipped in a big trailer (like in Cars, the movie). When we arrive in Oslo, will go to the hotel together (unless you have somewhere else to sleep), and have a \"quiet\" night in. The weekend will be dedicated to racing and some socializing.  A party can be guaranteed! Hope to see you there :)",
+		dato: "2019-11-15", tid: "18:00", sted: "Oslo", link:"javascript:void(0)", tag: "Competition"},
+	{type: "practice", title: "Group Practice #18", informasjon: "Today: Drifting. Instructor: Harald Gress",
+		dato: "2019-10-20", tid: "18:00", sted: "Gløshaugen", link:"javascript:void(0)", tag: "Practice"},
+	{type: "practice", title: "Group Practice #19", informasjon: "Today: Speed. Instructor: Harald Gress",
+		dato: "2019-10-23", tid: "18:00", sted: "Gløshaugen", link:"javascript:void(0)", tag: "Practice"},
+	{type: "practice", title: "Group Practice #20", informasjon: "Today: Tweaking. Instructor: Harald Gress",
+		dato: "2019-10-27", tid: "18:00", sted: "Gløshaugen", link:"javascript:void(0)", tag: "Practice"},
+	{type: "practice", title: "Group Practice #21", informasjon: "Today: Speed. Instructor: Harald Gress",
+		dato: "2019-10-30", tid: "18:00", sted: "Gløshaugen", link:"javascript:void(0)", tag: "Practice"},
+	{type: "practice", title: "Group Practice #22", informasjon: "Today: Drifting. Instructor: Harald Gress",
+		dato: "2019-11-03", tid: "18:00", sted: "Gløshaugen", link:"javascript:void(0)", tag: "Practice"},
+
 ];
+
 Date.prototype.getWeekNumber = function() {
   let d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
   let dayNum = d.getUTCDay() || 7;
