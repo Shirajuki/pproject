d88f43f57ee5923cfb6f3afd28351132c3250de5
ingsovik <ingsovik@users.noreply.github.com>
2019-11-01 21:21:14 +0100

    "fiksa" sortByDate og sortByAplhabet

--- js/events.js
+++ js/events.js
@@ -12,7 +12,7 @@ function skrivUtArtikkel(event) {
 		var mainText = calendar[i].informasjon;
 
 		//var liste = document.getElementById('liste');
-		var articleEl =  '<article><h2>'+title+'</h2><br> <p class="date">POSTED: '+date+'</p> <p class="type">TAG: '+type+'</p> <p class="mainText">'+mainText+'</p></article>';
+		var articleEl =  '<article><h2>'+title+'</h2><br> <p class="date">WHEN: '+date+'</p> <p class="type">TAG: '+type+'</p> <p class="mainText">'+mainText+'</p></article>';
 		var li = document.createElement('li');
 		li.innerHTML = articleEl;
 		document.getElementById('liste').prepend(li);
@@ -35,7 +35,7 @@ function sorterType(tag) {
 		var title = liTag[j].title;
 		var mainText = liTag[j].informasjon;
 
-		var articleEl =  '<article><h2>'+title+'</h2><br> <p class="date">POSTED: '+date+'</p> <p class="type">TAG: '+tag+'</p> <p class="mainText">'+mainText+'</p></article>';
+		var articleEl =  '<article><h2>'+title+'</h2><br> <p class="date">WHEN: '+date+'</p> <p class="type">TAG: '+tag+'</p> <p class="mainText">'+mainText+'</p></article>';
 		var liPostTag = document.createElement('li');
 		liPostTag.innerHTML = articleEl;
 		listeHTML.prepend(liPostTag);
@@ -52,8 +52,92 @@ sortSocialHTML.onclick = function(){sorterType('Social')};
 sortFundraiserHTML.onclick = function() { sorterType('Fundraiser')};
 sortCompetitionHTML.onclick = function () {sorterType('Competition')};
 
+/*var datList = [];
+for (i=0;i<calendar.length;i++){
+	var delDash = (calendar[i].dato.replace('-',''));
+	var dateNum = parseInt(delDash.replace('-',''));
+	datList.push(dateNum);
+
+}
+console.log(datList);*/
+
+function sortByDate2(list) {
+	return list.sort(function (a, b) {
+		return (new Date(a.dato)) - (new Date(b.dato))
+	})
+}
+
+//Funker ikke
+function sortByDate() {
+	datList.sort();
+	datList.reverse();
+
+
+	//console.log(datList);
+
+	var liDate = [];
+	for (i = 0; i < calendar.length; i++) {
+
+		if (datList[i] ==calendar[i].tag) {
+			liDate.push(calendar[i]);
+		}
+	}
+
+	var listeHTML = document.getElementById('liste');
+	listeHTML.innerHTML = '';
+	for (j = 0; j < liDate.length; j++) {
+
+		var tag = liDate[j].tag;
+		var date = liDate[j].dato;
+		var title = liDate[j].title;
+		var mainText = liDate[j].informasjon;
+
+		var articleEl =  '<article><h2>'+title+'</h2><br> <p class="date">WHEN: '+date+'</p> <p class="type">TAG: '+tag+'</p> <p class="mainText">'+mainText+'</p></article>';
+		var liPostDate = document.createElement('li');
+		liPostDate.innerHTML = articleEl;
+		listeHTML.prepend(liPostDate);
+	}
+
+}
+
+
+//funker ikke :(
+//sortDateHTML.addEventListener('click', sortByDate2(calendar));
+//Jukser, siden data nå sorteres automatisk setter jeg bare den normale laste funkjosnen
+//til å "sortere" etter dato
 var sortDateHTML = document.getElementById('sortDate');
-sortDateHTML.onclick = skrivUtArtikkel;
-//Hente ut knapp til å sortre etter type=fundraiser, og tildele funksjon
-var sortCompetitionHTML = document.getElementById('sortCompetition');
-// sortCompetitionHTML.addEventListener('click',sorterTypeCompetition);
+sortDateHTML.addEventListener('click', skrivUtArtikkel);
+
+//sort by alphabet
+
+
+function sortByAlpha() {
+	//Ikke helt sikker på hvordan denne funker hehe
+calendar.sort(function (a,b) {
+	var textA = a.title.toUpperCase();
+	var textB = b.title.toUpperCase();
+
+	//Vet ikke helt hva denne linjen gjør eller hvorfor den ser sånn ut
+	return (textA > textB) ? -1: (textA < textB) ? 1: 0;
+});
+	var liste = document.getElementById('liste');
+	liste.innerHTML = '';
+
+	for (i = 0; i < calendar.length; i++){
+		var type = calendar[i].tag;
+		var date = calendar[i].dato;
+		var title = calendar[i].title;
+		var mainText = calendar[i].informasjon;
+
+
+		var articleEl =  '<article><h2>'+title+'</h2><br> <p class="date">WHEN: '+date+'</p> <p class="type">TAG: '+type+'</p> <p class="mainText">'+mainText+'</p></article>';
+		var li = document.createElement('li');
+		li.innerHTML = articleEl;
+		liste.prepend(li);
+	}
+
+}
+
+var sortAlphHTML = document.getElementById('sortAlph');
+sortAlphHTML.addEventListener('click',sortByAlpha);
+
