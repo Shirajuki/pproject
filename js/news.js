9395f3273efd0f91d74520b1f076df958ab1004c
Synne Jenssen <synnej@stud.ntnu.no>
2019-11-03 19:38:31 +0100

    3 nyeste artiklene i news vises i navbar

--- js/news.js
+++ js/news.js
@@ -1,10 +1,9 @@
 //LIST OF ARTICLES
-////// SKAL OVERSETTE TIL ENGELSK - Synne //////
 const articles = [
   {
     title: "Welcome, new members!",
-    id_3char: "welcome_new_members", //makes it easier to create short-forms to use for id and anchor links //kan lage for loop som først bruker split or whatev på titleen og så setter de sammen med _
-    date: "15. September 2019",
+    id_3char: "welcome_new_members", //makes it easier to create short-forms to use for id and anchor links //other possible solution: using split to remove whitespace and characters that are not letter -> get first 6 letters to make id/anchor link
+    date: "15. September 2019", //chose to register date as just manual input string rather than the date (date obj.) it was "posted"/(added) because it was easier to write and the main focus of our site was elsewhere this time + didn't have time
     p_text: "Maecenas euismod porttitor magna, condimentum blandit eros eleifend sit amet. Aliquam pharetra, libero vitae auctor dictum, sapien orci mollis enim, sit amet auctor ipsum arcu non augue. Mauris eget auctor velit, at tincidunt sem. Quisque vel leo sed felis tincidunt molestie. Suspendisse scelerisque nunc vitae velit aliquam viverra. Pellentesque purus orci, eleifend et sagittis at, fringilla ac lorem. Aliquam erat volutpat. Nulla facilisi. Sed venenatis, nunc eget egestas fringilla, tellus nisl viverra nulla, fermentum faucibus enim lectus aliquam est. Duis sodales tellus sed ipsum condimentum, at cursus ligula porttitor. Duis cursus elit porta sem semper, id fermentum nibh dignissim. Mauris finibus dolor urna, id blandit sapien cursus non.",
   },
   {
@@ -32,7 +31,7 @@ const articles = [
     p_text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
-    title: "Competition qualification!!",
+    title: "Competition qualification",
     id_3char: "competition_qualification",
     date: "10. November 2019",
     p_text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -42,30 +41,20 @@ const articles = [
 
 //loop takes data from the set "articles" and prepends it to list in html as html-code
 window.onload = function(){
+  //prints the news articles: section and li-elements
   for(let i = 0; i<articles.length; i++){
-    //section and li-elements
     let li_element = document.createElement("li"); //declares new local variable, creates new element: li-node
     let sec_element = '<section class="newsarticle"><div class="news_heading"><h3 id="' + articles[i].id_3char +'">' + articles[i].title + '</h3><span class="span_date">' + articles[i].date + '</span></div><p>' + articles[i].p_text + '</p></section>'
-    li_element.innerHTML = sec_element;  //fills the li-element with input-element content
-    document.getElementById("list_with_articles").prepend(li_element); //writes to html-element with id list, prepends the node (new task) to the start of the list
-    //console.log(li_element);
-
-    //ENDRING: SLIK AT BARE DE 3 NYESTE VISES I NAVEN
-    //anchor links in sidebar
+    li_element.innerHTML = sec_element;  //fills the li-element with content
+    document.getElementById("list_with_articles").prepend(li_element); //writes to html-element with id list_with_articles, prepends the node to the start of the list
+  }
+  //prints the anchor links in sidebar, only the latest news
+  let navbar_list = document.getElementById("navbar_list")
+  let newest = articles.slice(-3) //the n latest articles (added to the bottom of the list "articles")
+  for(let j = 0; j<newest.length; j++){
     let li_nav_element = document.createElement("li"); //declares new local variable, creates new element: li-node
-    let a_element = '<a href="#' + articles[i].id_3char + '">' + articles[i].title + '</a>'
-    li_nav_element.innerHTML = a_element;  //fills the li-element with input-element content
-    document.getElementById("navbar_list").prepend(li_nav_element); //writes to html-element with id list, prepends the node (new task) to the start of the list
-    //console.log(li_element2);
+    let a_element = '<a href="#' + newest[j].id_3char + '">' + newest[j].title + '</a>'
+    li_nav_element.innerHTML = a_element;  //fills the li-element with content
+    navbar_list.prepend(li_nav_element); //writes to html-element with id navbar_list, prepends the node to the start of the list
   }
 }
-//chose to register date as just manual input string rather than the date (date obj.) it was "posted"/(added) because it was easier to write and the main focus of our site was elsewhere this time
-
-
-
-//burgermeny sidenav
-// function showNavbar(){
-//   let sidenav = document.getElementById("sidenav")
-//   sidenav.innerHTML='<div id="mobile"><div class=""><li><a href="spooky_scary.html"><br/>hei</a></li></div></div>'
-// }
-// showNavbar()
