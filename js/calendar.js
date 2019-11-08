b410dff7136c9578adfd33c9285f701552ded3b4
Jens <jenspoo@stud.ntnu>
2019-11-08 20:10:32 +0100

    minor changes -jens

--- js/calendar.js
+++ js/calendar.js
@@ -20,21 +20,21 @@ function loadCalendar(calendarList) {
 		if (datecheck != date) {
 			date = datecheck;
 			wrap = document.createElement('div');
-			wrap.className = 'calendar'
+			wrap.className = 'calendar';
 			wrap.innerHTML = `<div class="calendarDate"><p>${eldate.getDate()}/${eldate.getMonth() + 1}</p></div>`;
 			wrapList = document.createElement('ul');
 		}
 		const info = (el.info.length > 40 && el.type != 'practice') ? (el.info.slice(0,40)+'...') : (el.info);
 		wrapList.innerHTML += `<li class="${el.type}"><p><b>${el.type == 'event' ? `<a href="events.html#${el.link}" class="anchorCalendar">${el.title}</a>` : el.title + "<br>"}</b>${info}</p><p class="timeAndLocation">Time: ${el.time}<br>Location: ${el.location}</p></li>`;
-		wrap.appendChild(wrapList)
-		calendarId.appendChild(wrap)
+		wrap.appendChild(wrapList);
+		calendarId.appendChild(wrap);
 	}
 };
 
 function loadCheckboxes(calendar) {
-	const locationsfromdata = Array.from(new Set(calendar.map(x => x.location)))
+	const locationsfromdata = Array.from(new Set(calendar.map(x => x.location)));
 	for (const place of locationsfromdata){
-		const label = place.toLowerCase().replace(" ","")+"Checkbox"
+		const label = place.toLowerCase().replace(" ","") + "Checkbox";
 		locations.innerHTML += `<div class="input"><input id="${label}" type="checkbox" checked><label for="${label}">${place}</label></div>`;
 	}
 	const filter = document.querySelectorAll("#filter div input");
