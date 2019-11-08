36f7b6308f8e3cc4e4bfb0ecad517879fbf756ff
shirajuki <vulturegfd@reborn.com>
2019-11-08 19:39:21 +0100

    data type edit fix

--- js/calendar.js
+++ js/calendar.js
@@ -42,9 +42,7 @@ function loadCheckboxes(calendar) {
 		filter[i].addEventListener("change", () => {
 			let newCalendar = [...calendar];
 			for (let j = 0; j < filter.length; j++) {
-				if (!filter[j].checked) {
-					newCalendar = newCalendar.filter(el => !(filter[j].id.includes(el.type) || filter[j].id.includes(el.sted.toLowerCase())));
-				}
+				if (!filter[j].checked) newCalendar = newCalendar.filter(el => !(filter[j].id.includes(el.type) || filter[j].id.includes(el.sted.toLowerCase())));
 			}
 			loadCalendar(newCalendar);
 		});
