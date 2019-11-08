0c1350a96712c874580fa0a1e8d5c74224a945d3
Jens <jenspoo@stud.ntnu>
2019-11-08 20:51:43 +0100

    toned down blue color in calendar.html -jens

--- js/calendar.js
+++ js/calendar.js
@@ -1,16 +1,16 @@
-const locations = document.querySelector("#locationBox");
+const locationsId = document.querySelector("#locationBox");
 const calendarId = document.getElementById("calendarElements");
 window.addEventListener('load', () => {
-	loadCheckboxes(calendar);
+	loadFilterCheckboxes(calendar);
 	loadCalendar(calendar);
 });
 
-function loadCalendar(calendarList) {
-	calendarList = sortListByDate(calendarList).reverse();
+function loadCalendar(calendarData) { 
+	calendarData = sortListByDate(calendarData).reverse();
 	calendarId.innerHTML = '';
-	let week = 0, date = '', string = '';
-	let wrap, wrapList;
-	for (el of calendarList) {
+	let week = 0, date = '';
+	let wrapCalendarContent, wrapList;
+	for (el of calendarData) {
 		const eldate = new Date(el.date);
 		if (eldate.getWeekNumber() != week) {
 			week = eldate.getWeekNumber();
@@ -19,23 +19,23 @@ function loadCalendar(calendarList) {
 		const datecheck = `${eldate.getDate()}-${eldate.getMonth()}-${eldate.getFullYear()}`;
 		if (datecheck != date) {
 			date = datecheck;
-			wrap = document.createElement('div');
-			wrap.className = 'calendar';
-			wrap.innerHTML = `<div class="calendarDate"><p>${eldate.getDate()}/${eldate.getMonth() + 1}</p></div>`;
+			wrapCalendarContent = document.createElement('div');
+			wrapCalendarContent.className = 'calendarObject';
+			wrapCalendarContent.innerHTML = `<div class="calendarDate"><p>${eldate.getDate()}/${eldate.getMonth() + 1}</p></div>`;
 			wrapList = document.createElement('ul');
 		}
 		const info = (el.info.length > 40 && el.type != 'practice') ? (el.info.slice(0,40)+'...') : (el.info);
-		wrapList.innerHTML += `<li class="${el.type}"><p><b>${el.type == 'event' ? `<a href="events.html#${el.link}" class="anchorCalendar">${el.title}</a>` : el.title + "<br>"}</b>${info}</p><p class="timeAndLocation">Time: ${el.time}<br>Location: ${el.location}</p></li>`;
-		wrap.appendChild(wrapList);
-		calendarId.appendChild(wrap);
+		wrapList.innerHTML += `<li class="${el.type}Type"><p><b>${el.type == 'event' ? `<a href="events.html#${el.link}" class="anchorCalendar">${el.title}</a>` : el.title + "<br>"}</b>${info}</p><p class="timeAndLocation">Time: ${el.time}<br>Location: ${el.location}</p></li>`;
+		wrapCalendarContent.appendChild(wrapList);
+		calendarId.appendChild(wrapCalendarContent);
 	}
 };
 
-function loadCheckboxes(calendar) {
+function loadFilterCheckboxes(calendar) {
 	const locationsfromdata = Array.from(new Set(calendar.map(x => x.location)));
 	for (const place of locationsfromdata){
 		const label = place.toLowerCase().replace(" ","") + "Checkbox";
-		locations.innerHTML += `<div class="input"><input id="${label}" type="checkbox" checked><label for="${label}">${place}</label></div>`;
+		locationsId.innerHTML += `<div class="input"><input id="${label}" type="checkbox" checked><label for="${label}">${place}</label></div>`;
 	}
 	const filter = document.querySelectorAll("#filter div input");
 	for (let i = 0; i < filter.length; i++) {
